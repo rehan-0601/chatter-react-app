88b83f9b71ce5aa1dcc97ce8265194816a7ed326
Rehan Mulla <rmulla@splunk.com>
2020-07-08 18:52:14 -0700

    Updates

---
+++ precache-manifest.b56b8b4db2851da8a5964edcd3dac8af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdaa92ccabca70b9834ff76c032ce78a",
+    "url": "/chatter-react-app/index.html"
+  },
+  {
+    "revision": "196afc6700e512c5666e",
+    "url": "/chatter-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "452279af82a7d5d4fab4",
+    "url": "/chatter-react-app/static/js/2.9c89e876.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chatter-react-app/static/js/2.9c89e876.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "196afc6700e512c5666e",
+    "url": "/chatter-react-app/static/js/main.05c72aa2.chunk.js"
+  },
+  {
+    "revision": "c99deb84ba85c195bcdc",
+    "url": "/chatter-react-app/static/js/runtime-main.d812bcc3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chatter-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
